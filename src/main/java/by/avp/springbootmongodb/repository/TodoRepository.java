e973f763c34ebeaef39315af6d26f92de9b9ed75
avp02 <vpetranovskij@gmail.com>
2022-04-29 09:59:56 +0300

    added repository package

---
+++ src/main/java/by/avp/springbootmongodb/repository/TodoRepository.java
@@ -0,0 +1,11 @@
+package by.avp.springbootmongodb.repository;
+
+import by.avp.springbootmongodb.model.TodoDTO;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends MongoRepository<TodoDTO, String> {
+
+
+}
