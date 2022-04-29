af4cecb06526a6d45bd72e09ae3583aff1bddb36
avp02 <vpetranovskij@gmail.com>
2022-04-29 10:14:14 +0300

    added controller package

---
+++ src/main/java/by/avp/springbootmongodb/controller/TodoController.java
@@ -0,0 +1,30 @@
+package by.avp.springbootmongodb.controller;
+
+import by.avp.springbootmongodb.model.TodoDTO;
+import by.avp.springbootmongodb.repository.TodoRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TodoController {
+
+    private final TodoRepository todoRepository;
+
+    public TodoController(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    @GetMapping("/todos")
+    public ResponseEntity<?> findAllTodos() {
+        List<TodoDTO> todos = todoRepository.findAll();
+        if (todos.size() > 0) {
+            return new ResponseEntity<>(todos, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("No todos available", HttpStatus.NOT_FOUND);
+        }
+    }
+}
