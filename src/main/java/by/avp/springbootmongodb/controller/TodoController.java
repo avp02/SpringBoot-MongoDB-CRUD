d77d2315d90bac441759987a90422a29e97be72e
avp02 <vpetranovskij@gmail.com>
2022-05-01 18:06:57 +0300

    finished TodoController.java, Crud operations

--- src/main/java/by/avp/springbootmongodb/controller/TodoController.java
+++ src/main/java/by/avp/springbootmongodb/controller/TodoController.java
@@ -2,32 +2,76 @@ package by.avp.springbootmongodb.controller;
 
 import by.avp.springbootmongodb.model.TodoDTO;
 import by.avp.springbootmongodb.repository.TodoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TodoController {
 
-    @Autowired
-    private TodoRepository todoRepository;
-//    private final TodoRepository todoRepository;
+    private final TodoRepository todoRepository;
 
-//    public TodoController(TodoRepository todoRepository) {
-//        this.todoRepository = todoRepository;
-//    }
+    public TodoController(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
 
     @GetMapping("/todos")
     public ResponseEntity<?> findAllTodos() {
         List<TodoDTO> todos = todoRepository.findAll();
-        if (todos.size() > 0) {
-            return new ResponseEntity<>(todos, HttpStatus.OK);
+
+        return (todos.size() > 0) ? new ResponseEntity<>(todos, HttpStatus.OK) :
+                new ResponseEntity<>("No todos available", HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/todos")
+    public ResponseEntity<?> createTodo(@RequestBody TodoDTO todoDTO) {
+        try {
+            todoDTO.setCreatedAt(new Date(System.currentTimeMillis()));
+            todoRepository.save(todoDTO);
+            return new ResponseEntity<>(todoDTO, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/todos/{id}")
+    public ResponseEntity<?> getSingleTodoById(@PathVariable("id") String id) {
+        Optional<TodoDTO> todoOptional = todoRepository.findById(id);
+        return todoOptional.isPresent() ? new ResponseEntity<>(todoOptional.get(), HttpStatus.OK) :
+                new ResponseEntity<>("Not found todo with id = " + id, HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/todos/{id}")
+    public ResponseEntity<?> updateTodo(@PathVariable("id") String id, @RequestBody TodoDTO todoDTO) {
+        Optional<TodoDTO> todoOptional = todoRepository.findById(id);
+        if (todoOptional.isPresent()) {
+            TodoDTO todoToSave = todoOptional.get();
+            todoToSave.setTodo(todoDTO.getTodo() != null ? todoDTO.getTodo() : todoToSave.getTodo());
+            todoToSave.setDescriptions(todoDTO.getDescriptions() != null ? todoDTO.getDescriptions() :
+                    todoToSave.getDescriptions());
+            todoToSave.setCompleted(todoDTO.getCompleted() != null ? todoDTO.getCompleted() :
+                    todoToSave.getCompleted());
+            todoToSave.setCreatedAt(todoDTO.getCreatedAt() != null ? todoDTO.getCreatedAt() :
+                    todoToSave.getCreatedAt());
+            todoToSave.setUpdatedAt(new Date(System.currentTimeMillis()));
+            todoRepository.save(todoToSave);
+            return new ResponseEntity<>(todoToSave, HttpStatus.OK);
         } else {
-            return new ResponseEntity<>("No todos available", HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>("Not found todo with id = " + id, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/todos/{id}")
+    public ResponseEntity<?> deleteTodoById(@PathVariable("id") String id) {
+        try {
+            todoRepository.deleteById(id);
+            return new ResponseEntity<>("Todo was deleted successfully with id " + id, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
 }
